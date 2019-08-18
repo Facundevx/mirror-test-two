a85a44068d770540db687119ee71678e1e05a8c3
Facundevx <facundohsanchez@gmail.com>
2019-08-17 21:35:32 -0300

    Fixed get obj or 404

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
 
